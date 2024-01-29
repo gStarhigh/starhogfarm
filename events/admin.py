9325c2a6ce38a6884da5a38e29c1a4a9a53af713
gStarhigh <gustafstar@hotmail.com>
2024-01-29 23:21:20 +0100

    admin panel fix, fixa email i modell imorgon

--- events/admin.py
+++ events/admin.py
@@ -5,7 +5,11 @@ from .models import Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ('booking_id', 'full_name', 'phone_number', 'email',
                     'competition_level', 'agreement_accepted', 'deposit_paid',
-                    'full_payment_paid', 'created_at', 'updated_at')
-
+                    'full_payment_paid', 'created_at', 'updated_at',)
+    
+    list_filter = ('competition_level', 'agreement_accepted', 'deposit_paid',
+                    'full_payment_paid')
+# Måste lösa så att varje bokning blir märkt med till vilket event det är gjort.
+# måste ta bort email- unique i modellen.
 
 admin.site.register(Event, EventAdmin)
