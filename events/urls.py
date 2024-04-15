87e3231badda26bfd78fc6716e27a2f5906a0de8
gStarhigh <gustafstar@hotmail.com>
2024-04-15 21:56:12 +0200

    fixat superuserpage

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EventListCreateView, AdultEventListCreateView, KidsEventListCreateView, AvailableSpotsView, KidsAvailableSpotsView
+from .views import EventListCreateView, AdultEventListCreateView, KidsEventListCreateView, AvailableSpotsView, KidsAvailableSpotsView, BookingListView
 
 urlpatterns = [
     path('events/', EventListCreateView.as_view(), name='event-list'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('available_spots_adult/', AvailableSpotsView.as_view(), name='available-spots-adult'),
     path('kidsevent/', KidsEventListCreateView.as_view(), name='kids-event-list'),
     path('available_spots_kids/', KidsAvailableSpotsView.as_view(), name='available-spots-kids'),
+    path('bookings/', BookingListView.as_view(), name='booking-list'),
 ]
